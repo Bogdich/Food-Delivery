168ada64c9f85ea014a8570f48e17713bc66f9e9
eneguE <bogdevich96@hotmail.com>
2017-04-26 23:16:28 +0300

    DAO
    creating dao implementations

---
+++ Server/REST SERVER/src/main/java/com/dao/impl/CategoryDAOImpl.java
@@ -0,0 +1,9 @@
+package com.dao.impl;
+
+import com.bogdevich.catalog.dao.CategoryDAO;
+
+/**
+ * Created by Adrienne on 26.04.17.
+ */
+public class CategoryDAOImpl implements CategoryDAO {
+}
---
+++ Server/REST SERVER/src/main/java/com/dao/impl/DishDAOImpl.java
@@ -0,0 +1,7 @@
+package com.dao.impl;
+
+/**
+ * Created by Adrienne on 26.04.17.
+ */
+public class DishDAOImpl {
+}
